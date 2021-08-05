f5c7e62d017f229ed8519f319ca3382a4f2a5f80
Gurman61 <78517005+Gurman61@users.noreply.github.com>
2021-08-04 20:46:12 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -40,7 +40,7 @@ message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
 like_button="<button class='btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)' >";
 span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
 row=name_with_tag+message_with_tag+like_button+span_with_tag;
-document.getElementById("output").value+=row;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
